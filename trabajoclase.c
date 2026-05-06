6d9878195dfb36d54a100314285dda68fc9737f0
Laboratorio <laboratorio@udla-ec.int>
2026-05-06 15:09:03 -0500

    añadido los caracteres con los punteros

--- trabajoclase.c
+++ trabajoclase.c
@@ -13,9 +13,14 @@ int main() {
    float *ptrF;         
    ptrF = &pi;   
   decimal=*ptrF;
-   printf("%.3f %.3f %p",pi ,decimal ,ptrF);
+   printf("%.3f %.3f %p\n",pi ,decimal ,ptrF);
    
    
-
+   char letra= 'a', caracter;
+   char *ptrC;
+   ptrC=&letra;
+   caracter = *ptrC;
+   
+   printf("%c %c %p", letra, caracter, ptrC);
     return 0;
 }
